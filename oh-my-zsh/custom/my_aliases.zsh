9133d94c3ba40e6a355ecd5021ecd26c3facf5e7
Travis Elnicky <travis.elnicky@moneydesktop.com>
2013-09-05 09:31:18 -0600

    updated bundle update alias to use sublime

--- oh-my-zsh/custom/my_aliases.zsh
+++ oh-my-zsh/custom/my_aliases.zsh
@@ -13,7 +13,7 @@ alias kp="ps auxwww"
 # bundler aliases
 alias b="bundle"
 alias bi="b install --path vendor"
-alias bo="EDITOR=mate b open"
+alias bo="EDITOR=sub b open"
 alias bu="b update"
 alias bx="b exec"
 alias binit="bi && b package && echo 'vendor/ruby' >> .gitignore"
