9d6f95dfb595c0de0192c89a6afc1c5b35850966
Travis Elnicky <travis.elnicky@moneydesktop.com>
2013-10-23 09:53:14 -0600

    added zsh stat and upgrade functions

--- oh-my-zsh/custom/plugins/elnicky/elnicky.plugin.zsh
+++ oh-my-zsh/custom/plugins/elnicky/elnicky.plugin.zsh
@@ -1,3 +1,14 @@
 c() { cd ~/Code/$1; }
 _c() { _files -W ~/Code -/; }
 compdef _c c
+
+# autocorrect is more annoying than helpful
+unsetopt correct_all
+
+function upgrade_oh_my_zsh() {
+  /usr/bin/env ZSH=$ZSH /bin/sh $ZSH/tools/upgrade.sh
+}
+
+function zsh_stats() {
+  history | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n20
+}
