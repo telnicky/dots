543fd4dbcbbe39dd20e181e7cf10a0565f854f35
Travis Elnicky <telnicky@instructure.com>
2015-01-29 10:56:55 -0700

    Added wifi alias

--- oh-my-zsh/custom/my_aliases.zsh
+++ oh-my-zsh/custom/my_aliases.zsh
@@ -13,7 +13,7 @@ alias kp="ps auxwww"
 # bundler aliases
 alias b="bundle"
 alias bi="b install --path vendor"
-alias bo="EDITOR=sub b open"
+alias bo="EDITOR=vim b open"
 alias bu="b update"
 alias bx="b exec"
 alias binit="bi && b package && echo 'vendor/ruby' >> .gitignore"
@@ -48,4 +48,5 @@ alias pz='PB_CLIENT_TYPE=Zmq'
 # misc
 alias path='echo $PATH'
 alias reload='source ~/.zshrc'
+alias wifi='networksetup -setairportpower airport off; networksetup -setairportpower airport on'
 
